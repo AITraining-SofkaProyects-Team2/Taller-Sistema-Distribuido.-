8af7c8d293fb881ab2cd40406c0facb48e596821
Matias Nuniez <matiasnuniez192@gmail.com>
2026-02-20 15:05:08 -0300

    feat: filter component

--- frontend/src/pages/Dashboard/DashboardPage.tsx
+++ frontend/src/pages/Dashboard/DashboardPage.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import TicketList from "./TicketList";
 import Layout from "../../components/layout/Layout";
+import Filters from "./Filters";
 
 const DashboardPage: React.FC = () => {
   return (
     <Layout>
       <h1 className="text-3xl font-bold text-indigo-700 mb-8">Dashboard de Gestión de Tickets</h1>
+      <Filters />
       <TicketList />
     </Layout>
   );
